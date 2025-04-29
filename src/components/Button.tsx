e351cd75eb5fbff9733c33a432dcec466d24fe3e
santiago <spaloma.v1@gmail.com>
2025-04-29 13:37:59 -0700

    feat: basic setup for Landing

---
+++ src/components/Button.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./CustomButton.css";
+
+interface CustomButtonProps {
+  text: string;
+  onClick?: () => void;
+  className?: string;
+}
+
+const CustomButton: React.FC<CustomButtonProps> = ({
+  text,
+  onClick,
+  className,
+}) => {
+  return (
+    <button className={`custom-button ${className || ""}`} onClick={onClick}>
+      {text}
+    </button>
+  );
+};
+
+export default CustomButton;
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -17,24 +17,49 @@ export default function Landing() {
 
   return (
     <div className="landing-container">
-      <h1 className="landing-title">Where would you like to eat?</h1>
-
-      <input
-        type="text"
-        className="search-bar"
-        placeholder="Search restaurants, cuisine..."
-        value={searchText}
-        onChange={handleInputChange}
-        onKeyDown={handleKeyDown}
-      />
-
-      <section className="section one">
-        <h2>Popular Places</h2>
+      <section className="section hero">
+        <h1 className="landing-title">Where would you like to eat?</h1>
+        <input
+          type="text"
+          className="search-bar"
+          placeholder="Search restaurants, cuisine..."
+          value={searchText}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
+        />
       </section>
 
-      <section className="section two">
-        <h2>Recommended For You</h2>
+      <section className="section split-section left-image">
+        <div className="left-box">
+          <div className="grey-box" />
+        </div>
+        <div className="right-text">
+          <h2>Look up the menu ahead of time!</h2>
+          <p>More details here about the section yapyayapyapyap</p>
+        </div>
       </section>
+
+      <section className="section image-highlight">
+        <div className="image-overlay-text">
+          <h2>
+            Something Something uhhh idk but something interesting that catches
+            the eye of the customer
+          </h2>
+          <p>More details here about the section yapyayapyapyap</p>
+        </div>
+      </section>
+
+      <section className="section split-section right-image">
+        <div className="left-text">
+          <h2>Look up the menu ahead of time!</h2>
+          <p>More details here about the section yapyayapyapyap</p>
+        </div>
+        <div className="right-box">
+          <div className="grey-box" />
+        </div>
+      </section>
+
+      <section className="section footer-section"></section>
     </div>
   );
 }
