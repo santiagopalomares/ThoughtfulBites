96dc533824d8b2c4a4c8de16c50880335ee5ea34
santiago <spaloma.v1@gmail.com>
2025-04-30 11:13:47 -0700

    feat: conditional Navbar init, fix z-axis overlay

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,34 +1,51 @@
 import "./Navbar.css";
-import viteLogo from "../assets/react.svg";
-import { Link, useLocation } from "react-router-dom";
+import Logo from "../assets/Logo.png";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 export default function Navbar() {
   const location = useLocation();
+  const navigate = useNavigate();
+  const { isLoggedIn, logout } = useAuth();
 
   if (location.pathname === "/sign-up") {
     return null;
   }
 
+  const handleLogout = () => {
+    logout();
+    navigate("/"); // redirect to landing
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-left">
-        <img src={viteLogo} alt="Logo" className="logo" />
+        <img src={Logo} alt="Logo" className="logo" />
         <span className="site-name">ThoughtfulBites</span>
       </div>
 
       <div className="navbar-right">
-        <Link to="/about" className="nav-link">
+        <Link to="/about" className="nav-item">
           About
         </Link>
-        <Link to="/" className="nav-link">
-          Landing
+        <Link to="/contact" className="nav-item">
+          Contact
         </Link>
-        <Link to="/sign-up" className="nav-link">
-          Sign Up
-        </Link>
-        <Link to="/login" className="nav-link">
+        <Link to="/sign-up" className="nav-item">
           Login
         </Link>
+        {!isLoggedIn ? (
+          <Link to="/SignUp" className="nav-item nav-button">
+            SignUp
+          </Link>
+        ) : (
+          <button
+            onClick={handleLogout}
+            className="nav-item nav-button button-link"
+          >
+            Logout
+          </button>
+        )}
       </div>
     </nav>
   );
