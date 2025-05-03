9549f7c7adf6289186cfad05fff29ef205930275
Geric Capili <gericcapili@gmail.com>
2025-05-03 16:29:21 -0700

    fix: Finish cleanup

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -16,7 +16,7 @@ type FormData = {
 
 export default function ContactUs() {
   const [data, setData] = useState<FormData>(INITIAL_DATA);
-  const [errorMessage, setErrorMessage] = useState<String>("");
+  const [errorMessage, setErrorMessage] = useState<string>("");
 
   const { name, email, message } = data;
 
@@ -26,7 +26,7 @@ export default function ContactUs() {
     });
   }
 
-function onSubmit(e: FormEvent): void {
+  function onSubmit(e: FormEvent): void {
     e.preventDefault();
     if (!name.trim() || !email.trim()) {
         setErrorMessage("Please provide both your name and email.")
@@ -44,53 +44,58 @@ function onSubmit(e: FormEvent): void {
         <h1 className="landing-title">
           Contact Us
         </h1>
-        {/* Section 2 (Contact-Us Form) */}
+      {/* Contact-Us Form */}
       <section className="section split-section">
         <form className="contact-form" onSubmit={onSubmit}>
-                <div className="left-box">
-                    <input
-                        type="text"
-                        className="name-entry"
-                        placeholder="Name"
-                        value={name}
-                        onChange={(e) => updateFields({ name: e.target.value })}
-                        required
-                    />
-                    <input
-                        type="text"
-                        className="email-entry"
-                        placeholder="Email"
-                        value={email}
-                        onChange={(e) => updateFields({ email: e.target.value })}
-                        required
-                    />
-                    <textarea
-                        className="message-entry"
-                        placeholder="Message"
-                        value={message}
-                        onChange={(e) => updateFields({ message: e.target.value })}
-                        required
-                    />
-                    <button
-            type="submit"
-            className="send-button"
-            >Send</button>
+          <div className="left-box">
+            <input
+                type="text"
+                className="name-entry"
+                placeholder="Name"
+                value={name}
+                onChange={(e) => updateFields({ name: e.target.value })}
+                required
+            />
+            <input
+                type="text"
+                className="email-entry"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => updateFields({ email: e.target.value })}
+                required
+            />
+            <textarea
+                className="message-entry"
+                placeholder="Message"
+                value={message}
+                onChange={(e) => updateFields({ message: e.target.value })}
+                required
+            />
+            <button
+              type="submit"
+              className="send-button">
+                Send
+            </button>
             {errorMessage && <p className="error">{errorMessage}</p>}
-                </div>
+          </div>
         </form>
+        {/* Contact-Us Text */}
         <div className="right-box">
           <h2>Want to contact us?</h2>
-          <p>
-          Have questions, feedback, or need help finding the perfect restaurant for your dietary needs?
-          We're here to help!
-          Whether you're looking for more information about our platform, want to suggest a restaurant,
-          or just have something to share, feel free to reach out. Your experience matters to us.
+          <p style={{ whiteSpace: "pre-line" }}>
+          {
+            `Have questions, feedback, or need help finding the perfect restaurant for your dietary needs?
+
+            We're here to help!
+
+            Whether you're looking for more information about our platform, want to suggest a restaurant, or just have something to share, feel free to reach out.
+            Your experience matters to us!`
+          }
           </p>
         </div>
       </section>
-      </section>
-
-      <section className="section footer-section"></section>
+    </section>
+    <section className="section footer-section"></section>
     </div>
   );
 }
