3b648cef8a5a6df26efe5b6215cd7f44cb950e7d
Geric Capili <gericcapili@gmail.com>
2025-05-04 22:33:23 -0700

    fix: clean up

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,22 @@ import Landing from "./pages/Landing";
 import ContactUs from "./pages/ContactUs"
 import AboutUs from "./pages/AboutUs";
 import Navbar from "./components/Navbar";
+import { AuthProvider } from "./context/AuthContext";
 import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/contact-us" element={<ContactUs />} />
-        <Route path="/AboutUs" element={<AboutUs />} />
-        <Route path="/sign-up" element={<SignUp />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/AboutUs" element={<AboutUs />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
