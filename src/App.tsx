b05f775a51947fae5ab3d0822bf3ae70727ce39b
Geric Capili <gericcapili@gmail.com>
2025-05-06 13:01:19 -0700

    feat: Added Contact Us Page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUp from "./pages/SignUp";
 import Landing from "./pages/Landing";
 import AboutUs from "./pages/AboutUs";
+import ContactUs  from "./pages/ContactUs";
 import Search from "./pages/SearchResults";
 import Navbar from "./components/Navbar";
 import MenuOptions from "./pages/MenuOptions";
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/AboutUs" element={<AboutUs />} />
+          <Route path="/contact" element={<ContactUs />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/search" element={<Search />} />
           <Route path="/menu-options/:restaurantId" element={<MenuOptions />} />
---
+++ src/pages/ContactUs.tsx
@@ -0,0 +1,106 @@
+
+
+import { FormEvent, useState } from "react";
+import "./ContactUs.css";
+
+type FormData = {
+    name: string;
+    email: string;
+    message: string;
+  };
+  
+  const INITIAL_DATA: FormData = {
+    name: "",
+    email: "",
+    message: "",
+  };
+
+
+export default function ContactUs() {
+  const [data, setData] = useState<FormData>(INITIAL_DATA);
+  const [errorMessage, setErrorMessage] = useState<string>("");
+
+  const { name, email, message } = data;
+
+  function updateFields(fields: Partial<FormData>): void {
+    setData((prev) => {
+      return { ...prev, ...fields };
+    });
+  }
+
+  function onSubmit(e: FormEvent): void {
+    e.preventDefault();
+    if (!name.trim() || !email.trim()) {
+        setErrorMessage("Please provide both your name and email.")
+        return;
+    }
+
+    setErrorMessage("");
+    alert(`SUCCESS: Message sent by ${data.name}`); // TODO: Add endpoint here when ready
+    setData(INITIAL_DATA)
+    }
+
+  return (
+    <div className="landing-container">
+      <section className="section hero">
+        <h1 className="landing-title">
+          Contact Us
+        </h1>
+      {/* Contact-Us Form */}
+      <section className="section split-section">
+        <form className="contact-form" onSubmit={onSubmit}>
+          <div className="left-box">
+            <input
+                type="text"
+                className="name-entry"
+                placeholder="Name"
+                value={name}
+                onChange={(e) => updateFields({ name: e.target.value })}
+                required
+            />
+            <input
+                type="text"
+                className="email-entry"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => updateFields({ email: e.target.value })}
+                required
+            />
+            <textarea
+                className="message-entry"
+                placeholder="Message"
+                value={message}
+                onChange={(e) => updateFields({ message: e.target.value })}
+                required
+            />
+            <button
+              type="submit"
+              className="send-button">
+                Send
+            </button>
+            {errorMessage && <p className="error">{errorMessage}</p>}
+          </div>
+        </form>
+        {/* Contact-Us Text */}
+        <div className="right-box">
+          <h2>Want to contact us?</h2>
+          <p style={{ whiteSpace: "pre-line" }}>
+          {
+            `Have questions, feedback, or need help finding the perfect restaurant for your dietary needs?
+
+
+            We're here to help!
+
+            
+            Whether you're looking for more information about our platform, want to suggest a restaurant, or just have something to share, feel free to reach out.
+            Your experience matters to us!`
+          }
+          </p>
+        </div>
+      </section>
+    </section>
+    <section className="section footer-section"></section>
+    </div>
+  );
+}
+
