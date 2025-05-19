4f05a87d30d81327213d1a24e9ed772c27a36308
santiago <spaloma.v1@gmail.com>
2025-05-19 15:39:34 -0700

    feat: established connection between server and client

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
 
 app.get("/api", (req, res) => {
-  res.json({ example: ["data1", "data2", "data2"] });
+  res.json({ example: ["data1", "data2", "data3"] });
 });
 
 app.listen(8080, () => {
